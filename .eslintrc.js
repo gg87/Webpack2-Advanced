619450bb13e35e398c1951e9ac726d2191ae79a9
Giorgio Gamberoni <giorgio.gamberoni@gmail.com>
2017-03-08 16:26:46 +0100

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "env": {
+        'browser': true
+    },
+    "rules": {
+        "no-console": 0,
+        "comma-dangle": [2, "never"],
+        "max-len": [0],
+        "arrow-body-style": ["error", "as-needed"]
+    },
+    settings: {
+        "import/resolver": "webpack"
+    }
+};
